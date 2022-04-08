7969d744c1ab1345d1866749e5fed2b29beca7fe
gabriel <gabrielfalavinhagomes@gmail.com>
2022-04-08 20:41:18 -0300

    Adicao das activitys - Gabriel Gomes

---
+++ app/src/main/java/com/example/projeto_ds2/AvisoActivity.java
@@ -0,0 +1,14 @@
+package com.example.projeto_ds2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvisoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aviso);
+    }
+}
